67b678dbbc7bbea63d484ac58f38e3b1780a17a8
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-07 16:51:59 +0100

    new cake view working

--- controllers/cakes_controller.rb
+++ controllers/cakes_controller.rb
@@ -1,8 +1,21 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/cake.rb')
+require_relative('../models/baker.rb')
 
 get '/cakes' do
   @cakes = Cake.read_all()
   erb(:"cakes/index")
+end
+
+get '/cakes/new' do
+  @cakes = Cake.read_all()
+  @bakers = Baker.read_all()
+  erb(:"cakes/new")
+end
+
+post '/cakes/new' do
+  cake = Cake.new params
+  cake.save()
+  erb(:"cakes/success")
 end
