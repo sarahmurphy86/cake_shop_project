30c2d5333bbbc3661cd58a199100c79712cd14e1
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-07 10:55:32 +0100

    cake save method added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative "../models/cake.rb"
 require_relative "../models/baker.rb"
 
 Baker.delete_all()
@@ -12,3 +13,24 @@ baker2 = Baker.new ({
 
   baker1.save()
   baker2.save()
+
+cake1 = Cake.new ({
+  "name" => "Lemon Drizzle",
+  "description" => "Delicious lemon sponge soaked in zesty lemon sugar",
+  "quantity" => "10",
+  "buying_cost" => "5",
+  "retail_price" => "8",
+  "baker_id" => baker1.id
+  })
+
+cake2 = Cake.new ({
+  "name" => "Chocolate",
+  "description" => "Tasty chocolate sponge covered in a thick dark chocolate icing",
+  "quantity" => "3",
+  "buying_cost" => "8",
+  "retail_price" => "10",
+  "baker_id" => baker2.id
+  })
+
+  cake1.save()
+  cake2.save()
---
+++ models/cake.rb
@@ -0,0 +1,25 @@
+require_relative "../db/sql_runner"
+
+class Cake
+
+  attr_reader :id, :name, :description, :quantity, :buying_cost, :retail_price, :baker_id
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @name = options["name"]
+    @description = options["description"]
+    @quantity = options["quantity"]
+    @buying_cost = options["buying_cost"]
+    @retail_price = options["retail_price"]
+    @baker_id = options["baker_id"].to_i
+  end
+
+# CREATE METHOD - Individual
+  def save()
+    sql = "INSERT INTO cakes (name, description, quantity, buying_cost, retail_price, baker_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id"
+    values = [@name, @description, @quantity, @buying_cost, @retail_price, @baker_id]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]["id"].to_i
+  end
+
+end
