863bdbb8ab32dcc7f4e328c53a884d9390ba9615
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-09 10:25:25 +0100

    sql- table added to include cake type

--- db/cake_shop.sql
+++ db/cake_shop.sql
@@ -1,5 +1,11 @@
 DROP TABLE IF EXISTS cakes;
 DROP TABLE IF EXISTS bakers;
+-- DROP TABLE IF EXISTS cake_types;
+
+CREATE TABLE cake_types (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)not NULL
+);
 
 CREATE TABLE bakers (
   id SERIAL4 PRIMARY KEY,
@@ -13,5 +19,6 @@ CREATE TABLE cakes (
   quantity INT4 not NULL,
   buying_cost INT4,
   retail_price INT4,
-  baker_id INT4 REFERENCES bakers(id)
+  baker_id INT4 REFERENCES bakers(id),
+  cake_type_id INT4 REFERENCES cake_types(id)
 );
