73becd3977672cbdfd58c95432afda70a921baa6
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-09 10:43:48 +0100

    Seed data updated with cake type

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require_relative "../models/cake.rb"
 require_relative "../models/baker.rb"
+require_relative "../models/cake_type.rb"
+
 require( 'pry-byebug' )
 
 Cake.delete_all()
@@ -16,13 +18,35 @@ baker2 = Baker.new ({
   baker1.save()
   baker2.save()
 
+cake_type1 = Cake_type.new ({
+  "type" => "Classic Sponge"
+  })
+
+cake_type2 = Cake_type.new ({
+  "type" => "Wedding Cake"
+  })
+
+cake_type3 = Cake_type.new ({
+  "type" => "Birthday Cake"
+  })
+
+cake_type4 = Cake_type.new ({
+  "type" => "Cup Cake"
+  })
+
+  cake_type1.save()
+  cake_type2.save()
+  cake_type3.save()
+  cake_type4.save()
+
 cake1 = Cake.new ({
   "name" => "Lemon Drizzle",
   "description" => "Delicious lemon sponge soaked in zesty lemon sugar",
   "quantity" => "10",
   "buying_cost" => "5",
   "retail_price" => "8",
-  "baker_id" => baker1.id
+  "baker_id" => baker1.id,
+  "cake_type_id" => cake_type1.id
   })
 
 cake2 = Cake.new ({
@@ -31,7 +55,8 @@ cake2 = Cake.new ({
   "quantity" => "3",
   "buying_cost" => "8",
   "retail_price" => "10",
-  "baker_id" => baker2.id
+  "baker_id" => baker2.id,
+  "cake_type_id" =>cake_type3.id
   })
 
   cake1.save()
